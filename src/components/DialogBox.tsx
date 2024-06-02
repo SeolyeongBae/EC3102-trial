6f2e31bd8d999c147abbf37bbe787c984ab9037a
yeong <80435616+SeolyeongBae@users.noreply.github.com>
2024-06-02 16:29:23 +0900

    feat: 스타일 수정

--- src/components/DialogBox.tsx
+++ src/components/DialogBox.tsx
@@ -7,7 +7,7 @@ interface DialogBoxProps {
 
 const DialogBox: React.FC<DialogBoxProps> = ({ displayedText, speaker }) => {
   return (
-    <div className=" text-xl dialog-box p-2 bg-white rounded-lg shadow-md h-48 text-center">
+    <div className="text-xl dialog-box p-10 bg-slate-100 text-slate-800 rounded-lg shadow-md text-center ">
       <div className={`dialog-line ${speaker}`}>
         <strong>{speaker}: </strong>
         {displayedText}
